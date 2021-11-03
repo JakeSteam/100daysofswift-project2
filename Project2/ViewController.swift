5c44e58c64883fd437e47e6bae681440f995edf1
Jake Lee <jake.lee@photobox.com>
2021-11-03 20:21:11 +0000

    Day 19.2

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
 }
 
